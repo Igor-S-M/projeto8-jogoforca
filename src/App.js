02fbc2699ecc55669c8ddcb65559561af8168b2f
Igor Sousa <igorsousa3148@gmail.com>
2022-09-24 13:41:39 -0300

    pronto pra comecar os useState

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import React from "react"
+import palavras from "./palavras"
 import forca0 from "./image/forca0.png"
 /*
 import forca1 from "./image/forca1.png"
@@ -9,59 +11,63 @@ import forca6 from "./image/forca6.png"
 */
 
 export default function App() {
-    return (
-       <div className="hangman-game">
-            <Screen/>
-            <Keyboard/>
-            <Guess/>
-       </div>
-    )
-}
 
 
-
-function Screen() {
-    return (
-        <div className="screen">
-            <Image />
-            <StartButton />
-        </div>
-    )
-}
-
-function Image() {
-    return (
-
-        <img src={forca0} alt="imagem de forca" />
-
-    )
-}
-
-function StartButton() {
-    return (
-        <button className="start-button">Escolher Palavra</button>
-    )
-}
-
-
-
-function Keyboard() {
-
+    
+    //botoes do alfabeto
     const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
     return (
-        <div className="keyboard">
-            {alfabeto.map((i) => <button>{i.toUpperCase()}</button>)}
+        <div className="hangman-game">
+            <div className="screen">
+                <img src={forca0} alt="imagem de forca" />
+                <button onClick={start}>Escolher Palavra</button>
+                <Word />
+            </div>
+            <div className="keyboard">
+                {alfabeto.map((i) => <button>{i.toUpperCase()}</button>)}
+            </div>
+            <div className="guess">
+                <p>Já sei a palavra!</p>
+                <input></input>
+                <button>chutar</button>
+            </div>
         </div>
     )
 }
 
-function Guess() {
-    return (
-        <div className="guess">
-            <p>Já sei a palavra!</p>
-            <input></input>
-            <button>chutar</button>
-        </div>
-    )
+
+
+
+let palavra = ""
+let palavraCerta = ""
+
+function Word(flag) {
+
+    flag = true
+    if (flag === true) {
+        palavraCerta = palavras[Math.floor(Math.random() * palavras.length)]
+        for (let i in palavraCerta) {
+            palavra += "_ "
+        }
+
+
+        return (
+            <div className="word">
+                <p>{palavra}</p>
+                <p>{palavraCerta}</p>
+            </div>
+        )
+    } else {
+        return (
+            <></>
+        )
+    }
+}
+
+function start() {
+
+    console.log("start foi acionado")
+    alert("start foi acionado")
+
 }
